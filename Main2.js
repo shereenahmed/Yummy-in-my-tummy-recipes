94e5b1c122bd6976e9b031dc0aed2d6a8292aac3
BenjaminMK <richy2190@hotmail.com>
2016-03-04 11:34:23 +0100

    upload 1.1
    carrot cake upload

---
+++ Main2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Benjamin on 2016-03-02.
+ */
+
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    document.body.appendChild(btn);
